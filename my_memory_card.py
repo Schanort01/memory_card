46ffe5104f04058669506588bee0f4d92c4c2f4c
Schanort01 <78097004+Schanort01@users.noreply.github.com>
2021-05-08 10:15:14 +0300

    Add files via upload
    Проект приложения на PYQT5 - memory_card

---
+++ my_memory_card.py
@@ -0,0 +1,199 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QRadioButton,  
+        QPushButton, QLabel, QButtonGroup)
+ 
+app = QApplication([])
+ 
+# Создаем панель вопроса
+btn_OK = QPushButton('Ответить')
+lb_Question = QLabel('Самый сложный вопрос в мире!')
+ 
+RadioGroupBox = QGroupBox("Варианты ответов")
+ 
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
+ 
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3) # два ответа во второй столбец
+layout_ans3.addWidget(rbtn_4)
+ 
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+ 
+RadioGroupBox.setLayout(layout_ans1)
+ 
+# Создаем панель результата
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?') # здесь размещается надпись "правильно" или "неправильно"
+lb_Correct = QLabel('ответ будет тут!') # здесь будет написан текст правильного ответа
+ 
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+ 
+# Размещаем все виджеты в окне:
+layout_line1 = QHBoxLayout() # вопрос
+layout_line2 = QHBoxLayout() # варианты ответов или результат теста
+layout_line3 = QHBoxLayout() # кнопка "Ответить"
+ 
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+# Размещаем в одной строке обе панели, одна из них будет скрываться, другая показываться:
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+AnsGroupBox.hide() # эту панель мы уже видели, скроем, посмотрим, как получилась панель с ответом
+ 
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
+layout_line3.addStretch(1)
+ 
+# Теперь созданные строки разместим друг под другой:
+layout_card = QVBoxLayout()
+ 
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) # пробелы между содержимым
+##################################################
+from random import shuffle
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+class Question():
+    def __init__(self,question, right_answer, wrong1, wrong2, wrong3):
+        self.question = question
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+def ask(q: Question):
+    lb_Question.setText(q.question)
+    shuffle(answers)
+    answers[0].setText(q.right_answer)
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    lb_Correct.setText(q.right_answer)
+
+def check_answer():
+    if answers[0].isChecked():
+        show_correct('Правильно!')
+        window.score += 1
+    else:
+        show_correct('Неправильно!')
+    print('Рейтинг:', window.score / window.total *100, "%")
+
+
+def show_correct(res):
+    lb_Result.setText(res)
+    show_result()
+
+
+#####################################################
+def show_result():
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+
+RadioGroup = QButtonGroup()
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+def show_question():
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Ответить')
+
+    RadioGroup.setExclusive(False)
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True)
+    
+
+def click_OK():
+    if btn_OK.text() == 'Ответить':
+        check_answer()
+    else:
+        next_question()
+### ДЕНЬ 3 ###################################################
+question_list = []
+
+question_list.append(
+    Question('Государственный язык Бразилии',
+    'Бразильский', 
+    'Португальский', 'Итальянский', 'Испанский'))
+question_list.append(
+    Question('Государственный язык Португалии',
+    'Португальский', 
+    'Бразильский', 'Итальянский', 'Испанский'))
+question_list.append(
+    Question('Какого цвета нет на флаге России',
+    'Бирюзовый', 
+    'Белый', 'Синий', 'Красный'))
+question_list.append(
+    Question('В переводе с латинского языка слово "культура" означает...',
+    'передачу опыта и знаний', 
+    'возделывание, обработку земли', 'культ солнца', 'поклонение духам предков'))
+question_list.append(
+    Question('''Относительно независимое от природы 
+    духовно-материальное образование, порожденное
+     разнообразными формами совместной жизнедеятельности 
+     людей, называют...''',
+    'культурой', 
+    'формацией', 'расой', 'обществом'))
+question_list.append(
+    Question('Какого цвета нет на флаге Беларуси',
+    'Розовый', 
+    'Белый', 'Зеленый', 'Красный'))
+question_list.append(
+    Question('Как уходят по английски',
+    'Не прощаясь', 
+    'Громко крича', 'Поздоровавшись', 'Сказав чау'))  
+question_list.append(
+    Question('Столица Франции',
+    'Париж', 
+    'Филодельфия', 'Прованс', 'Марсель'))   
+question_list.append(
+    Question('Столица Австрии',
+    'Вена', 
+    'Брюсель', 'Прага', 'Берн'))   
+
+from random import randint
+
+def next_question():
+    window.total += 1
+    cur_question = randint(0, len(question_list)-1)
+    
+    ask(question_list[cur_question])
+    show_question()
+    print('Статистика')
+    print('-Всего вопросов:', window.total )
+    print('-Правильных ответов', window.score)
+
+ask(question_list[0])
+btn_OK.clicked.connect(click_OK)
+
+
+window = QWidget()
+window.score = 0
+window.total = 1
+window.cur_question = -1
+window.setLayout(layout_card)
+window.setWindowTitle('Memory Card')
+window.show()
+ 
+app.exec()
